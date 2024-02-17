56b66912a6df604f6e5fa4378480a1a46b73284c
Addison <zhenaddi@myvuw.ac.nz>
2024-02-17 23:14:10 +1300

    Setup basic layout transitions

--- main.py
+++ main.py
@@ -2,24 +2,42 @@ from queue import PriorityQueue
 from Board import Board
 import PySimpleGUI as sg
 
+sg.theme('DarkAmber')
+menuLayout = [[sg.Text("What would you like to do?")],
+              [sg.Button("Check-In Players")],  # Tick off players already in the system
+              [sg.Button("Register New Player")]]  # Add them to text file of players
+
+checkInLayout = [[sg.Text("Player Check Ins")]]
+
+# Create a (list) layout of layouts
+layout = [[sg.Column(menuLayout, visible=True, key='menu'),
+           sg.Column(checkInLayout, visible=False, key='check in')]]
+
+window = sg.Window("Badminton Board Maker", layout, size=(500, 500))
+
+
+def readPlayerFile():
+    # Read and process players.txt
+    playerFile = open('players.txt', 'r')
+    content = playerFile.readline()
+    print(content)
+
+
 if __name__ == '__main__':
-    # Set up Layout of the window
-    sg.theme('DarkAmber')
-    layout = [ [sg.Text("Some Text on row 1")],
-               [sg.Text('Ooga Booga'), sg.InputText()],
-               [sg.Button('OK'), sg.Button('Cancel')]]
+    # Create the Window using the main menu layout
 
-    # Create the Window
-    window = sg.Window("Window Title", layout)
     while True:
-
-        # Read player names txt file and pass them into the board
-        board = Board()
-        # board.test()
-
         event, values = window.read()
-        if event == sg.WIN_CLOSED or event == 'Cancel':
+        print(event, values)
+
+        if event in (None, 'Exit'):
             break
-        print("You entered ", values[0])
+        if event == 'Check-In Players':
+            # Change menu layout to invis and set check in layout to visible
+            window['menu'].update(visible=False)
+            layout = menuLayout
+            window['check in'].update(visible=True)
+
+            # Process actions here
 
     window.close()
